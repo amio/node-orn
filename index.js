0006db0111a96ee858351b1619b6bf80c0b56eaa
Amio <amio.cn@gmail.com>
2016-09-27 10:57:33 +0800

    Fix eslint error in tests.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #! /usr/bin/env node
-'use strict'
 
 const fs = require('fs')
 const chalk = require('chalk')
--- test/test.js
+++ test/test.js
@@ -5,10 +5,12 @@ const execFile = require('child_process').execFile
 const pkg = require('../package.json')
 const binPath = path.join(__dirname, '../', pkg.bin.orn)
 
+/* global describe, it */
+
 describe('bin', function () {
   it('should exit with status 1 if regex is invalid', function (cb) {
     const nodeArgs = [binPath, '/wrong-regex/format', '-d']
-    const cp = execFile('node', nodeArgs, function (error, stdout, stderr) {
+    execFile('node', nodeArgs, function (error, stdout, stderr) {
       assert.strictEqual(error && error.code, 1)
       cb()
     })
@@ -16,12 +18,11 @@ describe('bin', function () {
 
   it('should return the version', function (cb) {
     const cp = execFile('node', [binPath, '--version'])
-    const expected = pkg.version
+    const expected = 'v' + pkg.version
 
     cp.stdout.on('data', function (data) {
       assert.equal(data.replace(/\r\n|\n/g, ''), expected)
       cb()
     })
   })
-
 })
